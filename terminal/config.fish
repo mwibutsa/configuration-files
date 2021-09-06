c7424eb0d6e924cdfa65b6900dcd3c365c71ce12
mwibutsa <mflohost@gmail.com>
2021-09-06 14:23:18 +0200

    Add fish config file

---
+++ terminal/config.fish
@@ -0,0 +1,59 @@
+alias g++14="g++ -std=c++14 -Wall"
+alias g++17="g++ -std=c++17 -Wall"
+alias python="python3"
+alias pip="pip3"
+alias rndebugger='open "rndebugger://set-debugger-loc?host=localhost&port=8081"'
+export ANDROID_SDK_ROOT=/Users/codeevangelist/Library/Android/sdk/
+export ANDROID_HOME=/Users/codeevangelist/Library/Android/sdk/
+export ANDROID_SDK_HOME=~/.android/
+export ANDROID_AVD_HOME=~/.android/avd/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_301.jdk/Contents/Home
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/google-cloud-sdk/path.fish.inc'
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/codeevangelist/opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+# <<< conda initialize <<<
+alias g++14="g++ -std=c++14 -Wall"
+alias g++17="g++ -std=c++17 -Wall"
+alias python="python3"
+alias pip="pip3"
+alias rndebugger='open "rndebugger://set-debugger-loc?host=localhost&port=8081"'
+export ANDROID_SDK_ROOT=/Users/codeevangelist/Library/Android/sdk/
+export ANDROID_HOME=/Users/codeevangelist/Library/Android/sdk/
+export ANDROID_SDK_HOME=~/.android/
+export ANDROID_AVD_HOME=~/.android/avd/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_301.jdk/Contents/Home
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/google-cloud-sdk/path.fish.inc'
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/codeevangelist/opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+# <<< conda initialize <<<
+alias g++14="g++ -std=c++14 -Wall"
+alias g++17="g++ -std=c++17 -Wall"
+alias python="python3"
+alias pip="pip3"
+alias rndebugger='open "rndebugger://set-debugger-loc?host=localhost&port=8081"'
+export ANDROID_SDK_ROOT=/Users/codeevangelist/Library/Android/sdk/
+export ANDROID_HOME=/Users/codeevangelist/Library/Android/sdk/
+export ANDROID_SDK_HOME=~/.android/
+export ANDROID_AVD_HOME=~/.android/avd/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_301.jdk/Contents/Home
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/google-cloud-sdk/path.fish.inc'
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/codeevangelist/opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+# <<< conda initialize <<<
+#REMOVE FISH GREETING
+set fish_greeting
